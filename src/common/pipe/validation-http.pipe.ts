79cee8306b5dbccf5bcf511612c79738579a1bfe
xTCry [Vladislav Kh] <wolt6@mail.ru>
2022-04-19 01:30:15 +0300

    chore: using global `ValidationHttpPipe`

--- src/common/pipe/validation-http.pipe.ts
+++ src/common/pipe/validation-http.pipe.ts
@@ -1,44 +1,26 @@
 import {
     Injectable,
     PipeTransform,
-    ArgumentMetadata,
     BadRequestException,
+    ValidationPipe,
+    ValidationError,
 } from '@nestjs/common';
-import { validate } from 'class-validator';
-import { plainToClass } from 'class-transformer';
 
 @Injectable()
-export class ValidationHttpPipe implements PipeTransform<any> {
-    async transform(value: any, { metatype }: ArgumentMetadata) {
-        if (!metatype || !this.toValidate(metatype)) {
-            return value;
-        }
-
-        const entity = plainToClass(metatype, value);
-        const errors = await validate(entity);
-
-        if (errors.length > 0) {
-            throw new BadRequestException(
-                `Arguments invalid (${errors
-                    .map((err) => err.property)
-                    .join()}): ${this.formatErrors(errors)}`,
-            );
-        }
-        return entity || value;
-    }
-
-    private toValidate(metatype: any): boolean {
-        const types: any[] = [String, Boolean, Number, Array, Object];
-        return !types.includes(metatype);
-    }
-
-    private formatErrors(errors: any[]) {
-        return errors
-            .map((err) => {
-                for (const property in err.constraints) {
-                    return err.constraints[property];
-                }
-            })
-            .join(', ');
+export class ValidationHttpPipe
+    extends ValidationPipe
+    implements PipeTransform<any>
+{
+    createExceptionFactory() {
+        return (validationErrors: ValidationError[] = []) => {
+            throw new BadRequestException({
+                error: 'Bad Request',
+                message: 'Arguments invalid',
+                validation: validationErrors.map((err) => ({
+                    property: err.property,
+                    constraints: err.constraints,
+                })),
+            });
+        };
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import * as cors from 'cors';
 import helmet from 'helmet';
 
 import * as xEnv from '@my-environment';
-import { HttpExceptionFilter } from '@my-common';
+import { HttpExceptionFilter, ValidationHttpPipe } from '@my-common';
 
 import { AppModule } from './models/app/app.module';
 
@@ -31,6 +31,7 @@ async function bootstrap() {
     );
     app.use(json());
 
+    app.useGlobalPipes(new ValidationHttpPipe({ transform: true }));
     app.useGlobalFilters(new HttpExceptionFilter());
 
     app.use(requestIp.mw({ attributeName: 'ip' }));
